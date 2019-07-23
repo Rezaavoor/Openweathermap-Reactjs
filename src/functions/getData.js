7a923a0c9589f60a694e94157297da7e9832f6a0
Hosseini Reza <reza.hosseini@edu.nacka.se>
2019-07-23 09:58:17 +0200

    add get geolocation of the user

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -7,7 +7,6 @@ const WEATHERAPI = process.env.REACT_APP_WEATHERAPI
 exports.handler = async (event, context) => {
   const { lat, lng } = event.queryStringParameters
   const url = `${API_ENDPOINT}/data/2.5/forecast?lat=${lat}&lon=${lng}&mode=json&APPID=${WEATHERAPI}`
-  console.log(`url${url}url`)
   let data = ''
   data = await axios.get(url).then(json => json.data)
 
--- src/utils/WeatherContext.jsx
+++ src/utils/WeatherContext.jsx
@@ -3,11 +3,7 @@ import axios from 'axios'
 export const WeatherContext = createContext()
 
 export const WeatherProvider = props => {
-  const lat = 59.28867
-  const lng = 18.03322
-
-  useEffect(() => {
-    console.log('context useEffect')
+  const getData = (lat, lng) => {
     axios
       .get(`/.netlify/functions/getData?lat=${lat}&lng=${lng}`)
       .then(json => {
@@ -24,27 +20,28 @@ export const WeatherProvider = props => {
         setWeather(theWeather)
       })
       .catch(ex => console.log(ex))
-    // axios
-    //   .get(url)
-    //   .then(json => {
-    //     const data = json.data
-    //     console.log('context')
-    //     console.log(data)
-    //     const theWeather = {
-    //       ...weather,
-    //       loading: false,
-    //       city: data.city.name,
-    //       country: data.city.country,
-    //     }
-    //     const forecasts = data.list.slice(0, 9)
-    //     theWeather.forecasts = forecasts
-    //     setWeather(theWeather)
-    //   })
-    //   .catch(ex => console.log(ex))
-  }, [])
+  }
+
   const [weather, setWeather] = useState({
     loading: true,
   })
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords
+          getData(latitude, longitude)
+        },
+        () => {
+          console.log(`couldn't get your location`)
+          getData(51.509865, -0.118092)
+        }
+      )
+    } else {
+      console.log(`Your browser doesn't support geolocation`)
+      getData(51.509865, -0.118092)
+    }
+  }, [])
 
   return (
     <WeatherContext.Provider value={[weather, setWeather]}>
